b45d6956acfe3542f2695d9cd93b0316f14b46ff
Rxwyo <rxwyo@github.com>
2026-06-17 05:41:21 +0800

    Fix: Switch to Groovy DSL (build.gradle) to avoid Kotlin DSL plugin version issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
