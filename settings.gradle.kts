df78d0638e0506b73b63037deaf15dcf5ee3949a
Rxwyo <rxwyo@github.com>
2026-06-16 22:57:41 +0800

    Fix: Use forcedModules in settings.gradle.kts for Kotlin version resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,12 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+    
+    forcedModules = listOf(
+        "org.jetbrains.kotlin:kotlin-stdlib:1.8.22",
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22",
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.22"
+    )
 }
 
 rootProject.name = "TrafficKiller"
